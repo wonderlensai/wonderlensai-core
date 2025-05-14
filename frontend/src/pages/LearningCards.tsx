97827831646cce3e29f70dfe8dd2583d9475cf9d
ipldata25 <ipldata25@gmail.com>
2025-05-15 03:11:46 +0530

    font

--- frontend/src/pages/LearningCards.tsx
+++ frontend/src/pages/LearningCards.tsx
@@ -237,7 +237,6 @@ const LearningCards = () => {
           </Box>
         ) : (
           <motion.div
-            ref={cardRef}
             {...handlers}
             initial={{ opacity: 0, scale: 0.95 }}
             animate={{ opacity: 1, scale: 1 }}
