0afaf8dee6cd170479440007c6a2ad5eb5f6fcbc
ipldata25 <ipldata25@gmail.com>
2025-05-15 03:15:13 +0530

    font

--- frontend/src/pages/Scan.tsx
+++ frontend/src/pages/Scan.tsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import {
   Box,
   Container,
