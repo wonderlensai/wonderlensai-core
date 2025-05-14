af82c6ea0f1b5706e96ba3e40d2831b83f21f2f0
ipldata25 <ipldata25@gmail.com>
2025-05-15 03:02:41 +0530

    font

---
+++ frontend/src/pages/Learn.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from '@mui/material';
+
+const Learn = () => (
+  <Box sx={{ p: 4 }}>
+    <Typography variant="h3" color="primary">Learn</Typography>
+    <Typography variant="body1" sx={{ mt: 2 }}>
+      This is the new Learn page. Add your features here!
+    </Typography>
+  </Box>
+);
+
+export default Learn; 
